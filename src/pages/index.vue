789ccaed70f2717ba4d662480fc75940dda9bdb5
PETIT-Hugo <hugo.petit@edu.univ-fcomte.fr>
2024-04-09 16:41:58 +0200

    montrer 1 ou 2

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,9 +5,11 @@ const maVar = ref(true)
 </script>
 
 <template>
-  <button @click="maVar = false">- Cacher -</button>
-  <button @click="maVar = true">- Montrer -</button>
+  <button @click="maVar = 1">- Montrer 1 -</button>
+  <button @click="maVar = 2">- Montrer 2 -</button>
   <button @click="maVar = !maVar">- Bascule -</button>
-  <h1 v-show="maVar"
+  <h1 v-show="maVar === 1"
   class="text-2xl">En fait au début...</h1>
+  <h1 v-show="maVar === 2"
+  class="text-2xl">On l'a fait pour la culture</h1>
 </template>
