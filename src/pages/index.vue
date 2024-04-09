3455abff4d164ff3020f8431ab476ec9ba1c0f2f
PETIT-Hugo <hugo.petit@edu.univ-fcomte.fr>
2024-04-09 16:39:37 +0200

    ajout bouton bascule

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,8 +5,9 @@ const maVar = ref(true)
 </script>
 
 <template>
-  <button @click="maVar = false">Cacher</button>
-  <button @click="maVar = true">Montrer</button>
+  <button @click="maVar = false">- Cacher -</button>
+  <button @click="maVar = true">- Montrer -</button>
+  <button @click="maVar = !maVar">- Bascule -</button>
   <h1 v-show="maVar"
   class="text-2xl">En fait au début...</h1>
 </template>
