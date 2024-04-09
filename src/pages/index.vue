ff989a0aea03c93ed1646063b8191e4674715207
PETIT-Hugo <hugo.petit@edu.univ-fcomte.fr>
2024-04-09 16:46:17 +0200

    ajout image

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,11 +5,13 @@ const maVar = ref(true)
 </script>
 
 <template>
-  <button @click="maVar = 1">- Montrer 1 -</button>
-  <button @click="maVar = 2">- Montrer 2 -</button>
-  <button @click="maVar = !maVar">- Bascule -</button>
+  <p>la variable vaut : {{ maVar }}</p>
+  <button @click="maVar = 1">Montrer 1</button>
+  <button @click="maVar = 2">Montrer 2</button>
+  <button @click="maVar = !maVar">Cacher</button>
   <h1 v-show="maVar === 1"
   class="text-2xl">En fait au début...</h1>
   <h1 v-show="maVar === 2"
   class="text-2xl">On l'a fait pour la culture</h1>
+  <img src="https://www.aufoindelarue.com/wp-content/uploads/2024/01/La-Feve.jpg">
 </template>
