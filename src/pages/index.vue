9e1f4ea0d843db26d27f72831b0c959035e82de6
PETIT-Hugo <hugo.petit@edu.univ-fcomte.fr>
2024-04-09 16:37:54 +0200

    ajout des boutons wazink

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-//
+import { ref } from 'vue';
+
+const maVar = ref(true)
 </script>
 
 <template>
-  <h1 class="text-2xl">Bonjour monde !</h1>
+  <button @click="maVar = false">Cacher</button>
+  <button @click="maVar = true">Montrer</button>
+  <h1 v-show="maVar"
+  class="text-2xl">En fait au début...</h1>
 </template>
