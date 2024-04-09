279b5a3dce36656ac35719d5c1baf630b586a182
PETIT-Hugo <hugo.petit@edu.univ-fcomte.fr>
2024-04-09 16:54:08 +0200

    toggle

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,12 +6,20 @@ const maVar = ref(true)
 
 <template>
   <p>la variable vaut : {{ maVar }}</p>
-  <button @click="maVar = 1">Montrer 1</button>
-  <button @click="maVar = 2">Montrer 2</button>
+  <button @click="maVar = maVar === 1 ? 0 : 1">Montrer 1</button>
+
+  <button @click="maVar = maVar === 2 ? 0 : 2">Montrer 2</button>
+
   <button @click="maVar = !maVar">Cacher</button>
+
+
   <h1 v-show="maVar === 1"
   class="text-2xl">En fait au début...</h1>
+
   <h1 v-show="maVar === 2"
   class="text-2xl">On l'a fait pour la culture</h1>
-  <img src="https://www.aufoindelarue.com/wp-content/uploads/2024/01/La-Feve.jpg">
+
+  <img v-show="maVar === 1 || 2" src="https://www.aufoindelarue.com/wp-content/uploads/2024/01/La-Feve.jpg">
+
+
 </template>
