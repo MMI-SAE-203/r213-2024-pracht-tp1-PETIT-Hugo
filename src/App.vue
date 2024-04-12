a5ef86dc3c28873e5eca7228ee4be2f979900938
PETIT-Hugo <hugo.petit@edu.univ-fcomte.fr>
2024-04-12 14:02:27 +0200

    ajout de la variable v-show et test avec vuedevtools

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,29 @@
 <script setup lang="ts">
-import { onErrorCaptured } from 'vue'
+import { onErrorCaptured, ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
   return true
 })
+const menuIsOpen = ref(false)
 </script>
 
 <template>
   <header>
-    <nav>
-      <ul>
-        <li>
-          <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/accordeon" class="text-red-500 underline"> Accordéon </RouterLink>
-        </li>
-      </ul>
-    </nav>
     <button
     aria-controls="mainNav"
     aria-expanded="true"
     class="rounded-full border-2 border-red-600 bg-red-300 px-2"
+    
   >
     menu
   </button>
   <!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
-  <nav id="mainNav">
+  <nav id="mainNav" v-show="menuIsOpen">
     <ul>
-      <li><RouterLink to="@/pages/index.vue">Accueil</RouterLink></li>
-      <li><RouteurLink to="@/pages/accordeon.vue">Accordéon</RouteurLink></li>
+      <li><RouterLink to="/index.vue">Accueil</RouterLink></li>
+      <li><RouteurLink to="/accordeon.vue">Accordéon</RouteurLink></li>
     </ul>
   </nav>
   </header>
